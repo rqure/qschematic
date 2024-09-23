6d44e46af498c09bf67aa55f97a4ab99f19b3fe5
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-09-22 22:30:53 -0600

    some progress

--- web/js/main.js
+++ web/js/main.js
@@ -10,11 +10,14 @@ async function main() {
     const map = new SimpleMap('schematic');
     
     map.setBoundary({x: 0, y: 0}, {x: 1000, y: 1000}).moveTo(map.center);
-    map.draw(
-        new Schematic("home", context.qDatabaseInteractor)
-    );
+    // map.draw(
+    //     new Schematic("home", context.qDatabaseInteractor)
+    // );
 
     context.qDatabaseInteractor.runInBackground(true);
 
     CURRENT_LOG_LEVEL=LOG_LEVELS.DEBUG;
+
+    const dataManager = new DataManager(context.qDatabaseInteractor);
+    console.log(dataManager.findSchematic("home"));
 }
