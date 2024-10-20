db08fe0f2126c42bd76c7a7d6f09e44aa533e9ec
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2024-10-20 21:06:28 +0000

    Adjust import order

--- web/js/components/schematic_explorer.js
+++ web/js/components/schematic_explorer.js
@@ -8,30 +8,28 @@ function registerSchematicExplorerComponent(app, context) {
         },
 
         template: `
-            <div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item active">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">Models</h5>
-                            <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#new-model-modal">New</button>
-                        </div>
-                    </li>
-                    <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-start" v-for="model in models">
-                        {{ model }}
-                        <span class="badge text-bg-secondary" @click="onDeleteModel(model)">🗑</span>
-                    </li>
-                    <li class="list-group-item active">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">Schematics</h5>
-                            <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#new-schematic-modal">New</button>
-                        </div>
-                    </li>
-                    <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-start" v-for="schematic in schematics">
-                        {{ schematic }}
-                        <span class="badge text-bg-secondary" @click="onDeleteSchematic(schematic)">🗑</span>
-                    </li>
-                </ul>
-            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item active">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">Models</h5>
+                        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#new-model-modal">New</button>
+                    </div>
+                </li>
+                <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-start" v-for="model in models">
+                    {{ model }}
+                    <span class="badge text-bg-secondary" @click="onDeleteModel(model)">🗑</span>
+                </li>
+                <li class="list-group-item active">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">Schematics</h5>
+                        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#new-schematic-modal">New</button>
+                    </div>
+                </li>
+                <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-start" v-for="schematic in schematics">
+                    {{ schematic }}
+                    <span class="badge text-bg-secondary" @click="onDeleteSchematic(schematic)">🗑</span>
+                </li>
+            </ul>
         `
     });
 }
